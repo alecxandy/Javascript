0476d97594f40aa16c1a5bfaf2d4900a6f83a6e0
alexandre gomes <alecxandy@gmail.com>
2022-05-11 20:36:53 -0300

    template string e ternario

---
+++ template-string.js
@@ -0,0 +1,8 @@
+let nome = 'alexandre'
+let sobrenome = 'gomes'
+
+console.log('nome: ' + nome)
+console.log('sobrenome: ' + sobrenome)
+
+//templete string
+console.log(`nome: ${nome} ${sobrenome}`)
---
+++ ternario.js
@@ -0,0 +1,17 @@
+let isvalid = true;
+
+
+
+let zero = 0
+    //com ternario
+const resultado = zero > 0 ? 0 : -1
+
+//arrow function
+const resltsoma = function(x, y) {
+    return x + y;
+}
+
+//arrow function mais simplificada
+const dividir = (x, y) => {
+    return x / y;
+}
